92904dbf7ef0fe9d3dab7ca4a9466e5e3ca16a80
Paweł Grzesiak <pawel.grzesiak0@gmail.com>
2018-03-28 21:46:06 +0200

    location PUT method + config file

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    db_url: 'mongodb://localhost',
+    db_port: ':27017'
+};
--- routes/locations.js
+++ routes/locations.js
@@ -1,12 +1,12 @@
+var config = require('../config/config.js');
 var express = require('express');
 var app = express.Router();
-var mongodb = require('mongodb').MongoClient;
 var methodOverride = require('method-override');
 
 /* Handle DELETE/PUT data */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride(function(req, res){
+app.use(methodOverride(function(req, res) {
       if (req.body && typeof req.body === 'object' && '_method' in req.body) {
         var method = req.body._method
         delete req.body._method
@@ -17,7 +17,7 @@ app.use(methodOverride(function(req, res){
 /* GET locations listing */
 app.get('/', function(req, res, next) {
   var mongodb = require('mongodb').MongoClient;
-  mongodb.connect('mongodb://localhost:27017', function(err, client) {
+  mongodb.connect(config.db_url + config.db_port, function(err, client) {
     if (err) throw err;
     var dbo = client.db('gp_db');
     dbo.collection('locations').find().project({ _id: 0 }).toArray(function(err, result) {
@@ -35,7 +35,7 @@ app.get('/', function(req, res, next) {
 /* GET specific location */
 app.get('/:id', function(req, res, next) {
   var mongodb = require('mongodb').MongoClient;
-  mongodb.connect('mongodb://localhost:27017', function(error, client) {
+  mongodb.connect(config.db_url + config.db_port, function(error, client) {
     if (error) return next(error);
     var dbo = client.db('gp_db');
     dbo.collection('locations').find({ id: req.params.id }).project({ _id: 0 }).toArray(function(error, result) {
@@ -62,7 +62,7 @@ app.get('/:id', function(req, res, next) {
 app.post('/add', function(req, res, next) {
   if (!req.body.loc_id || !req.body.loc_name) return next(new Error('No data provided.'));
   var mongodb = require('mongodb').MongoClient;
-  mongodb.connect('mongodb://localhost:27017', function(error, client) {
+  mongodb.connect(config.db_url + config.db_port, function(error, client) {
     if (error) return next(error);
     var dbo = client.db('gp_db');
     dbo.collection('locations').insertOne({
@@ -81,7 +81,7 @@ app.post('/add', function(req, res, next) {
 app.delete('/:id', function(req, res, next) {
   if (!req.params.id) return next(new Error('No ID provided.'));
   var mongodb = require('mongodb').MongoClient;
-  mongodb.connect('mongodb://localhost:27017', function(error, client) {
+  mongodb.connect(config.db_url + config.db_port, function(error, client) {
     if (error) return next(error);
     var dbo = client.db('gp_db');
     dbo.collection('locations').remove({ id: req.params.id }, true, function(error, location) {
@@ -95,7 +95,24 @@ app.delete('/:id', function(req, res, next) {
 
 /* PUT location */
 app.put('/:id', function(req, res, next) {
-  res.send('mod');
+  if (!req.body.loc_id || !req.body.loc_name) return next(new Error('No data provided.'));
+  if (!req.params.id) return next(new Error('No Location ID passed.'));
+  var mongodb = require('mongodb').MongoClient;
+  mongodb.connect(config.db_url + config.db_port, function(error, client) {
+    if (error) return next(error);
+    var dbo = client.db('gp_db');
+    dbo.collection('locations').update(
+      { id: req.params.id }, 
+      {
+        id: req.body.loc_id,
+        name: req.body.loc_name
+      }, function(error, location) {
+        if (error) return next(error);
+        if (!location) return next(new Error('Failed to update Location.'));
+        res.redirect('/locations');
+      });
+    client.close();
+  });
 });
 
 module.exports = app;
